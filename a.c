3ac34d927bd98e9f36e354a99733f89d028ca949
ShyrushShrestha <Shyrush.sresta1@gmail.com>
2016-10-21 11:30:06 +0545

    Add files via upload

---
+++ a.c
@@ -0,0 +1,24 @@
+/*
+Student Name : Shyrush Shrestha
+Subject: Programming Fundamentals
+Roll No:
+Lab No :
+Program:
+*/
+ #include<stdio.h>
+ //#include<conio.h>
+	int main (){
+		int a,b;
+		
+		printf("\n Enter first number:\n");
+		scanf("%d",&a);
+		//printf("\n Enter second number:\n");
+		//scanf("%d",&b)
+		
+		if (a%2==0)
+		printf("\n number is even=%d\n",a);
+		else
+		printf("'n number is odd=%d\n",a);
+				
+		return (0);
+		}
