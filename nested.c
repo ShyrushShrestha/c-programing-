f21c942082fbc2d2223556536725345135554f50
ShyrushShrestha <Shyrush.sresta1@gmail.com>
2016-10-21 12:00:10 +0545

    Add files via upload

---
+++ nested.c
@@ -0,0 +1,34 @@
+/*
+Student Name : Shyrush Shrestha
+Subject: Programming Fundamentals
+Roll No:
+Lab No :
+Program:
+*/
+
+ #include<stdio.h>
+ //#include<conio.h>
+ 
+		int main (){
+		int a,b,c;
+		printf("\n Enter first number: \n");
+		scanf("%d",&a);
+		printf("\n Enter second number: \n");
+		scanf("%d",&b);
+		printf("\n Enter third number: \n");
+		scanf("%d",&c);
+		
+		
+	if (a=b)
+	{
+		if (a>c){
+			printf("%d is greatest: \n",a);
+	}
+	else {
+		printf("%d is greatest: \n",c);
+	}
+}
+		
+
+			 return(0);
+		}
