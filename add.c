412f42c62cd2f8a6b0dfe8b7366d26fa3bf9ebd0
ShyrushShrestha <Shyrush.sresta1@gmail.com>
2016-11-18 10:17:40 +0545

    Add files via upload

---
+++ add.c
@@ -0,0 +1,20 @@
+/*
+Student Name: Shyrush Shrestha
+Roll:
+Lab no. :01
+Program: To print sm of two nos asked from users
+Date: 18 November, 2016
+*/
+#include<stdio.h>
+//#include<conio.h>
+ int main () {
+	 int a,b,sum;
+	 printf("Enter first number:");
+	 scanf("%d",&a);
+	 printf("Enter second number:");
+	 scanf("%d",&b);
+	 sum=a+b;
+	 printf("\n %d + %d = %d",a,b,sum );
+	 return (0);
+	 }
+
