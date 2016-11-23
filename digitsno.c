4db3b5db05a11396fda1ff7dcab8265387c5b3d5
ShyrushShrestha <Shyrush.sresta1@gmail.com>
2016-11-23 10:12:06 +0545

    Add files via upload

---
+++ digitsno.c
@@ -0,0 +1,23 @@
+/*
+Student Name : Ashis Shrestha
+Subject : Programming Fundamentals
+Lab No :
+Program : WAP to find sum of digits of three digits number.
+Roll No :
+Date : 23 November,2017
+*/
+#include<stdio.h>
+#include<conio.h>
+int main (){
+    int a,b,c,sum;
+printf("Enter the first number\n");
+scanf("%d",&a);
+printf("Enter the second number\n");
+scanf("%d", &b);
+printf("Enter the third number\n");
+scanf("%d", &c);
+sum=a+b+c;
+printf("\n %d + %d + %d = %d",a,b,c,sum );
+
+
+}
