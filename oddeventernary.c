b651e2da624d5ed9ab1af2b904fac86add7b955b
ShyrushShrestha <Shyrush.sresta1@gmail.com>
2016-12-07 11:50:54 +0545

    Add files via upload

---
+++ oddeventernary.c
@@ -0,0 +1,18 @@
+/* 
+Student Name : Ashis Shrestha
+Lab Sheet No:
+Program : WAP to check no is odd or even
+Date : 6 Dec 2016
+*/
+#include <stdio.h>
+//#include <conio.h>
+
+int main ()
+{
+int a;
+ printf("Enter the number:\n");
+ scanf("%d",&a);
+(a%2==0)?printf("No is even"):printf("No is odd");
+
+return 0;
+}
