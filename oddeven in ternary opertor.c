7dcbd9a35b5fa0cb0fdd17933a96e04cbc29f210
ShyrushShrestha <Shyrush.sresta1@gmail.com>
2016-12-09 10:35:17 +0545

    Add files via upload

---
+++ oddeven in ternary opertor.c
@@ -0,0 +1,20 @@
+/*
+Student name : Shyrush Shrestha
+Program :WAP to check odd/even no using t
+Lab Sheet No:12
+Roll No : 06
+Subject : Programming Fundamental
+*/
+
+#include <stdio.h>
+//#include <conio.h>
+
+int main()
+{
+int a;
+printf("Enter the number:");
+scanf("%d",&a);
+
+(a%2==0)?printf("The number is even",a):printf("The number is odd",a);
+return 0;
+}
