b3a59be5f52fc2d11e00b909ca2843a5a95e82f2
Shyrush Shrestha <Shyrush.sresta1@gmail.com>
2017-01-08 10:57:21 +0545

    Add files via upload

---
+++ leapyearternary.c
@@ -0,0 +1,22 @@
+/*
+Name 	: Ashis Shrestha
+Subject : Programming Fundamental
+Lab No	: 16
+Program : WAP to check whether year is leap year or not using coditional/ternary operator.
+Date 	: 8 jan 2017
+*/
+
+#include<stdio.h>
+
+int main ()
+{
+ int a;
+ 
+ printf("Enter the year :");
+ scanf("%d",&a);
+ 
+ ((a%4==0||a%400==0)&&(a%100!=0))?printf("%d is the leap year",a):printf("%d is not the leap year",a);
+ 
+ return 0;
+ 
+}
