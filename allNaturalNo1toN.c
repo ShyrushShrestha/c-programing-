3226553d555bc8771342b431600c9849cd0e6c02
Shyrush Shrestha <Shyrush.sresta1@gmail.com>
2017-01-08 13:05:32 +0545

    Add files via upload

---
+++ allNaturalNo1toN.c
@@ -0,0 +1,22 @@
+/*
+Name 	: Ashis Shrestha
+Subject : Programming Fundamental
+Lab No	: 16
+Program : WAP to print all natural number from 1 to n using while loop.
+Date 	: 8 jan 2017
+*/
+
+#include<stdio.h>
+int main ()
+{
+ int i;
+ printf("Enter any number :");
+ scanf("%d",&i);
+ while(i<100)
+ {
+	 
+	 printf("\n%d",i);
+	 i++;
+}
+ return 0;
+}
