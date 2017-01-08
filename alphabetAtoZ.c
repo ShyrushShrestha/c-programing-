360af1822c491504e1126e0542d9f8baef904d12
Shyrush Shrestha <Shyrush.sresta1@gmail.com>
2017-01-08 13:06:12 +0545

    Add files via upload

---
+++ alphabetAtoZ.c
@@ -0,0 +1,29 @@
+/*
+Name 	: Ashis Shrestha
+Subject : Programming Fundamental
+Lab No	: 16
+Program : WAP to print all natural number from 1 to n using while loop.
+Date 	: 8 jan 2017
+*/
+#include<stdio.h>
+
+int main ()
+{
+	char a=65;
+	
+	while (a<=90)
+	{
+	printf("\t%c",a);	
+	a++;
+	}
+	printf("\n \n");
+	a=97;
+	while (a<=122)
+	{
+	printf("\t%c",a);
+	a++;	
+	}
+	
+	return 0;
+
+}
