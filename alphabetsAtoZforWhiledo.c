22d5fc7817e470ebc7665f58cde484a98d41d602
Shyrush Shrestha <Shyrush.sresta1@gmail.com>
2017-01-12 10:17:48 +0545

    Add files via upload

--- alphabetsAtoZforWhiledo.c
+++ alphabetsAtoZforWhiledo.c
@@ -2,7 +2,7 @@
 Name 	: Ashis Shrestha
 Subject : Programming Fundamental
 Lab No	: 17
-Program : WAP to print all alphabets from a to z using for,while and do while loop.
+Program : WAP to print all alphabets from a to z using for loop.
 Date 	: 11 jan 2017
 */
 
@@ -14,6 +14,13 @@ Date 	: 11 jan 2017
 	 printf("Enter any alphabet:");
 	 scanf("%c",&a);
 	 for (a=97;a<=122;a++)
+	 {
+		printf("\t%c",a); 
+	}
+	
+	printf("\n\n");
+	
+	for (a=65;a<=90;a++)
 	 {
 		printf("\t%c",a); 
 	}
