0e1e4a0ef3a5e94fd98a87d552df5bdd9946c740
Shyrush Shrestha <Shyrush.sresta1@gmail.com>
2017-01-12 10:13:08 +0545

    Add files via upload

---
+++ AlphabetsFromAtoZWhileLoopNEXT.c
@@ -0,0 +1,21 @@
+/*
+Student Name : Aashish Prajapati
+Subject : Programming Fundamentals
+Roll No : 03
+Program : Write a C program to print all alphabets from a to z using while loop
+Lab Sheet No : 16
+Date : 08 January 2016
+*/
+
+#include<stdio.h>
+
+int main()
+{
+	int ASCII;
+	char ch;
+	ASCII = ch;
+	while (ASCII<=90||ASCII<=122)
+	{
+		ASCII = ch;
+		printf("%c",&ch) ;
+	
