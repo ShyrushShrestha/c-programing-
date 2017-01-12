8ba089724d03dee58494e16f20159289d3e8225a
Shyrush Shrestha <Shyrush.sresta1@gmail.com>
2017-01-12 10:48:52 +0545

    Add files via upload

---
+++ evenNo1to100forloop.c
@@ -0,0 +1,20 @@
+/*
+Name 	: Ashis Shrestha
+Subject : Programming Fundamental
+Lab No	: 17
+Program : WAP to print all the even numbers from 1 to 100 using for loop.
+Date 	: 11 jan 2017
+*/
+
+#include<stdio.h>
+
+int main ()
+{
+	int a;
+	
+	for (a=2;a<=100;a=a+2)
+	{
+		printf("\t%d",a);
+	}
+	return 0 ;
+}
